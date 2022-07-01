61f34099568a6b063aa698e3fe8c174a1e742d4b
Alex Krantz <alex@krantz.dev>
2022-07-01 16:59:08 -0700

    Add links kv binding

---
+++ manage/app/lib/types.ts
@@ -0,0 +1,17 @@
+import type { AppData, DataFunctionArgs } from '@remix-run/cloudflare';
+
+interface AppContext {
+  links: KVNamespace;
+}
+
+interface FunctionArgs extends DataFunctionArgs {
+  context: AppContext;
+}
+
+export interface ActionFunction {
+  (args: FunctionArgs): Promise<Response> | Response | Promise<AppData> | AppData;
+}
+
+export interface LoaderFunction {
+  (args: FunctionArgs): Promise<Response> | Response | Promise<AppData> | AppData;
+}
--- manage/server.ts
+++ manage/server.ts
@@ -1,13 +1,17 @@
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 import * as build from '@remix-run/dev/server-build';
 
-const handleRequest = createPagesFunctionHandler({
+interface Environment {
+  links: KVNamespace;
+}
+
+const handleRequest = createPagesFunctionHandler<Environment>({
   build,
   // eslint-disable-next-line no-undef
   mode: process.env.NODE_ENV,
   getLoadContext: (context) => context.env,
 });
 
-export function onRequest(context) {
+export function onRequest(context: EventContext<Environment, any, any>) {
   return handleRequest(context);
 }
