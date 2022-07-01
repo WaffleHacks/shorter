30ef7b34883c924c4409cf6bbaf35796688df81a
Alex Krantz <alex@krantz.dev>
2022-07-01 11:52:32 -0700

    Add link retrieval and redirection

--- director/src/index.ts
+++ director/src/index.ts
@@ -1,5 +1,19 @@
+import Link from "@/link";
+
 async function handle(request: Request, { LINKS }: Bindings): Promise<Response> {
-  return new Response("hello");
+  // Get the link id
+  const { pathname } = new URL(request.url);
+  const id = decodeURIComponent(pathname.substring(1));
+
+  // Retrieve the link
+  const link = await LINKS.get<Link>(id, { type: "json" });
+  if (link === null || !link.enabled) return new Response("not found", { status: 404 });
+
+  // Increment the usages
+  link.usages += 1;
+  await LINKS.put(id, JSON.stringify(link));
+
+  return Response.redirect(link.url);
 }
 
 const worker: ExportedHandler<Bindings> = { fetch: handle };
