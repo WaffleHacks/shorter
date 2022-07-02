add525c9b295ba320b6119ea0ffcd927a757dcfd
Alex Krantz <alex@krantz.dev>
2022-07-01 21:33:36 -0700

    Add tailwindcss

--- manage/app/root.tsx
+++ manage/app/root.tsx
@@ -1,5 +1,9 @@
-import type { MetaFunction } from '@remix-run/cloudflare';
+import type { LinksFunction, MetaFunction } from '@remix-run/cloudflare';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+
+import styles from '~/tailwind.css';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
--- manage/app/routes/index.tsx
+++ manage/app/routes/index.tsx
@@ -1,20 +1,35 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
+    <div className="font-sans leading-5">
+      <h1 className="text-3xl font-bold">Welcome to Remix</h1>
+      <ul className="list-disc list-inside">
         <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+            className="text-blue-500 underline hover:no-underline"
+          >
             15m Quickstart Blog Tutorial
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+            className="text-blue-500 underline hover:no-underline"
+          >
             Deep Dive Jokes App Tutorial
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer"
+            className="text-blue-500 underline hover:no-underline"
+          >
             Remix Docs
           </a>
         </li>
